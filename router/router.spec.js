23e87c1fe4501a4d0fdccd5606d19205f28ad19c
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-04-12 11:50:44 -0400

    Wrote out helper methods for router

--- router/router.spec.js
+++ router/router.spec.js
@@ -4,54 +4,62 @@ const db = require("../data/dbConfig.js");
 
 describe("server.js", () => {
   beforeEach(async () => {
-    await db("posts").truncate();
+    await db("games").truncate();
   });
-  describe("GET /games", async() => {
-    it("Should return status code 200 OK", () => {
-        const response = await request(server).get("/api/games");
-        expect(response.status).toBe(200)
-    })
+  describe("GET /games", () => {
+    it("Should return status code 200 OK", async () => {
+      const response = await request(server).get("/api/games");
+      expect(response.status).toBe(200);
+    });
     it("should return json", async () => {
-        const response = await request(server).get("/api/games");
-        expect(response.type).toBe("application/json");
+      const response = await request(server).get("/api/games");
+      expect(response.type).toBe("application/json");
     });
     it("should return an array", async () => {
-        const response = await request(server).get("/api/games");
-        expect(Array.isArray(response.body)).toBeTruthy();
-      });
-  })
-  describe("POST /games", async() => {
-      it("should return status code 201 if successful", async() => {
-          const response = await request(server).post('/api/games').send({
-            title: 'Pacman',
-            genre: 'Arcade', 
-            releaseYear: 1980 
-          });
-          expect(response.status).toBe(201);
-      })
-      it("should return json if successful", async () => {
-        const response = await request(server).post('/api/games').send({
-            title: 'Pacman',
-            genre: 'Arcade', 
-            releaseYear: 1980 
-          });
-          expect(response.type).toBe("application/json");
-      })
-      it("should return created object if successful", async () => {
-        const response = await request(server).post('/api/games').send({
-            title: 'Pacman',
-            genre: 'Arcade', 
-            releaseYear: 1980 
-          });
-          expect(typeof response).toBe("Object");
-          expect(response.title).toBe("Pacman")
-    })
-      it("should fail with status code 422 if information is incomplete", async() => {
-        const response = await request(server).post('/api/games').send({
-            genre: 'Arcade', 
-            releaseYear: 1980 
-          });
-          expect(response.status).toBe(422)
-      })
-  })
+      const response = await request(server).get("/api/games");
+      expect(Array.isArray(response.body)).toBeTruthy();
+    });
+  });
+  describe("POST /games", () => {
+    it("should return status code 201 if successful", async () => {
+      const response = await request(server)
+        .post("/api/games")
+        .send({
+          title: "Pacman",
+          genre: "Arcade",
+          releaseYear: 1980
+        });
+      expect(response.status).toBe(201);
+    });
+    it("should return json if successful", async () => {
+      const response = await request(server)
+        .post("/api/games")
+        .send({
+          title: "Pacman",
+          genre: "Arcade",
+          releaseYear: 1980
+        });
+      expect(response.type).toBe("application/json");
+    });
+    it("should return created object if successful", async () => {
+      const response = await request(server)
+        .post("/api/games")
+        .send({
+          title: "Pacman",
+          genre: "Arcade",
+          releaseYear: 1980
+        });
+      expect(typeof response).toBe("object");
+      expect(response.body.title).toBe("Pacman");
+    });
+    it("should fail with status code 422 if information is incomplete", async () => {
+      const response = await request(server)
+        .post("/api/games")
+        .send({
+          genre: "Arcade",
+          releaseYear: 1980
+        });
+      expect(response.status).toBe(422);
+    });
+  });
 });
